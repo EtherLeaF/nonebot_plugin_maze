8afe8cd5fde041be839527022c93d7c6570153c3
EtherLeaF <thetapilla@gmail.com>
2022-05-15 20:17:00 +0800

    v0.1.1 update

--- setup.py
+++ setup.py
@@ -1,13 +1,18 @@
 import setuptools
 
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setuptools.setup(
     name="nonebot_plugin_maze",
-    version="0.1.0",
+    version="0.1.1",
     author="EtherLeaF",
     author_email="thetapilla@gmail.com",
     keywords=["pip", "nonebot2", "nonebot", "nonebot_plugin"],
     description="""NoneBot2交互式走迷宫插件""",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url="https://github.com/EtherLeaF/nonebot_plugin_maze",
     packages=setuptools.find_packages(),
     classifiers=[
@@ -17,6 +22,6 @@ setuptools.setup(
     ],
     include_package_data=True,
     platforms="any",
-    install_requires=['nonebot-adapter-onebot>=2.0.0-beta.1', 'nonebot2>=2.0.0-beta.1', 'pillow>=8.0.0'],
+    install_requires=['nonebot-adapter-onebot>=2.0.0-beta.1', 'nonebot2>=2.0.0-beta.1', 'pillow>=7.0.0'],
     python_requires=">=3.8"
 )
